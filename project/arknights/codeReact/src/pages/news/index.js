2ce4ecc15a4a3ae0e96960f913b808a165925433
tanmutian <tanmutian@126.com>
2021-07-18 10:47:23 +0800

    2021.7.18

---
+++ project/arknights/codeReact/src/pages/news/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import {connect} from 'dva';
+import { DatePicker } from 'antd';
+import styles from './index.less';
+import notice from '../../image/notice.png'
+
+
+@connect(({news}) => ({news}))
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		const { news } = this.props;
+		return (
+			<div className={styles.global1}>
+				<div className={styles.yellow}>
+				</div>
+				
+				
+				<div className={styles.global2}>
+					<div className={styles.title}>
+						Latest News
+					</div>
+				</div>
+				
+				<div className={styles.totalArea}>
+					<div className={styles.date}>
+						February 21, 2021
+					</div>
+				
+					<div className={styles.line}>
+					</div>
+					
+					
+					
+					<div className={styles.noticeArea}>
+					
+						<img className={styles.imageRight} src={notice}/>
+						
+						
+						<div className={styles.textRight}>
+							Message from the production team #9
+						</div>
+						
+						<div className={styles.clear}>
+						</div>
+						
+					</div>
+					
+					
+					<div className={styles.clear}>
+					</div>
+				</div>
+				
+				<div className={styles.totalArea}>
+					<div className={styles.dateLeft}>
+						January 11, 2021
+					</div>
+				
+					<div className={styles.lineLeft}>
+					</div>
+					
+					<div className={styles.noticeArea}>
+					
+						<img className={styles.imageLeft} src={notice}/>
+						
+						<div className={styles.messageLeft}>
+							Message from the production team #8
+						</div>
+						
+						<div className={styles.clear}>
+						</div>
+						
+					</div>
+					
+					
+					<div className={styles.clear}>
+					</div>
+				</div>
+				
+				<div className={styles.totalArea}>
+					<div className={styles.date}>
+						December 03, 2020
+					</div>
+				
+					<div className={styles.line}>
+					</div>
+					
+					<div className={styles.noticeArea}>
+					
+						<img className={styles.imageRight} src={notice}/>
+						
+						<div className={styles.textRight}>
+							Message from the production team #7
+						</div>
+						
+						<div className={styles.clear}>
+						</div>
+						
+					</div>
+					
+					
+					<div className={styles.clear}>
+					</div>
+				</div>
+				
+				<div className={styles.clear}>
+				</div>
+			</div>
+		)
+	}
+}
+export default App;
+    
---
+++ project/arknights/codeReact/src/pages/news/model.js
@@ -0,0 +1,10 @@
+export default {
+    namespace: 'news',
+    state: {
+    },
+    reducers: {
+    },
+    effects: {
+    },
+    subscriptions: {},
+};
