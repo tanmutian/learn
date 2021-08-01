4b2b4a5f8abb78fb203bdba3f19b217c84d1fcec
tanmutian <tanmutian@126.com>
2021-08-01 10:16:03 +0800

    2021.8.1

--- project/arknights/codeReact/src/pages/user/index.js
+++ project/arknights/codeReact/src/pages/user/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Button,Tooltip } from 'antd';
+import { Button,Tooltip ,Radio} from 'antd';
 import { SearchOutlined } from '@ant-design/icons'
+import { PoweroffOutlined } from '@ant-design/icons'
 import styles from './index.less';
 
 @connect(({ user }) => ({ user }))
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state={
+            value1:1,
+        }
     }
 
+    onChange1 (e){
+        console.log('radio checked', e.target.value);
+        this.setState({
+            value1:e.target.value
+        });
+    };
+
     render() {
+        let  self=this;
         const { user } = this.props;
         return (
             <div className={styles.user_global}>
@@ -44,6 +56,33 @@ class App extends React.Component {
                     <Button type="primary" block>
                         Click me
                     </Button>
+                </div>
+                <div className={styles.button1}>
+                    <Button type="ghost">
+                        hided
+                    </Button>
+                </div>
+                <div className={styles.button1}>
+                    <Button type="primary" loading>
+                        please wait
+                    </Button>
+                </div>
+                <div className={styles.button1}>
+                    <Button type="primary" icon={<PoweroffOutlined />} loading >
+                    </Button>
+                </div>
+                <div className={styles.button1}>
+                    <Radio>
+                        choose me
+                    </Radio>
+                </div>
+                <div className={styles.button1}>
+                <Radio.Group onChange={self.onChange1.bind(self)} value={this.state.value1}>
+                    <Radio value={1}>A</Radio>
+                    <Radio value={2}>B</Radio>
+                    <Radio value={3}>C</Radio>
+                    <Radio value={4}>D</Radio>
+                </Radio.Group>
                 </div>
             </div>
         );
