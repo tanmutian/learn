26d950d3f8fe17fa02516ef36acd0d3a60083014
tanmutian <tanmutian@126.com>
2021-08-01 11:31:41 +0800

    2021.8.1

--- project/arknights/codeReact/src/pages/rhodesIland/index.js
+++ project/arknights/codeReact/src/pages/rhodesIland/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { connect } from 'dva';
-import { DatePicker } from 'antd';
+import { DatePicker,Carousel } from 'antd';
 import styles from './index.less';
 import city5 from '../../image/city5.png';
+
+const contentStyle = {
+    width:'200px',
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+    color:'red'
+  };
 
 @connect(({ rhodesIland }) => ({ rhodesIland }))
 class App extends React.Component {
@@ -17,14 +27,37 @@ class App extends React.Component {
                 <div className={styles.yellowPart}>
                 </div>
                 
+                <Carousel autoplay>
+                    <div>
+                        <h1 style={contentStyle}>
+                            1
+                        </h1>
+                    </div>
+                    <div>
+                        <h1 style={contentStyle}>
+                            2
+                        </h1>
+                    </div>
+                    <div>
+                        <h1 style={contentStyle}>
+                            3
+                        </h1>
+                    </div>
+                    <div>
+                        <h1 style={contentStyle}>
+                            4
+                        </h1>
+                    </div>
+                </Carousel>
                 <div className={styles.text}>
                 </div>
                 
                 <div className={styles.centralPicture}>
                 </div>
                 
-                <img className={styles.logoStyle} src={city5} />
-                
+                <div className={styles.logoStyle}>
+                    <img className={styles.imgArea} src={city5} />
+                </div>
                 
                 <div className={styles.allCharacter}>
                     <div className={styles.arrow}>
