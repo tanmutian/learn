fad4c289068198d819f194cd01a90830fc274c28
tanmutian <tanmutian@126.com>
2021-09-21 10:38:30 +0800

    2021.9.21

--- project/arknights/codeReact/src/pages/characterMain/index.js
+++ project/arknights/codeReact/src/pages/characterMain/index.js
@@ -7,92 +7,16 @@ import longMen from '../../image/longMen.png'
 import rhineLab from '../../image/rhineLab.png'
 import penguinLogistics from '../../image/penguinLogistics.png'
 import { history } from 'umi';
-import Test from '../../libs/test/test.js';
+import Navigation from '../../libs/navigation/navigation.js';
 
 //@connect(({characterMain}) => ({characterMain}))
 const CharacterMain =()=>{
-    const historyLatestEvents =()=>{
-        history.push('/latestEvents')
-    }
 
-    const historyNews =()=>{
-        history.push('/News')
-    }
-
-    const historyAnnouncement =()=>{
-        history.push('/announcement')
-    }
-
-    const historyRhodesIland =()=>{
-        history.push('/rhodesIland')
-    }
-
-    const menuEvents = (
-        <Menu>
-            <Menu.Item>
-            <div onClick={historyLatestEvents} className={styles.dropDown} target="_blank" rel="noopener noreferrer">
-                Latest Events
-            </div>
-            </Menu.Item>
-            <Menu.Item>
-            <div onClick={historyNews} className={styles.dropDown} target="_blank" rel="noopener noreferrer" >
-                News
-            </div>
-            </Menu.Item>
-            <Menu.Item>
-            <div onClick={historyAnnouncement} className={styles.dropDown} target="_blank" rel="noopener noreferrer" >
-                announcement
-            </div>
-            </Menu.Item>
-        </Menu>
-    );
-
-    const menuCharacter = (
-        <Menu>
-            <Menu.Item>
-            <div onClick={historyRhodesIland} className={styles.dropDown} target="_blank" rel="noopener noreferrer">
-                RhodesIsLand
-            </div>
-            </Menu.Item>
-            <Menu.Item>
-            <div className={styles.dropDown} target="_blank" rel="noopener noreferrer" >
-                Longmen
-            </div>
-            </Menu.Item>
-            <Menu.Item>
-            <div className={styles.dropDown} target="_blank" rel="noopener noreferrer" >
-                Rhine Lab
-            </div>
-            </Menu.Item>
-            <Menu.Item>
-            <div className={styles.dropDown} target="_blank" rel="noopener noreferrer" >
-                Penguin Logistics
-            </div>
-            </Menu.Item>
-        </Menu>
-    );
     
 
     return (
         <div className={styles.global}>
-            <div className={styles.yellow}>
-                <Dropdown overlay={menuEvents} placement="bottomCenter">
-                    <div className={styles.content1}>
-                        Events and news
-                    </div>
-                </Dropdown>
-
-                <div className={styles.content2}>
-                     Background
-                </div>
-
-                <Dropdown overlay={menuCharacter} placement="bottomCenter">
-                    <div className={styles.content2}>
-                        Operator data
-                    </div>
-                </Dropdown> 
-            </div>
-            <Test />
+            <Navigation />
             <div className={styles.area1}>
                 <div className={styles.picture1}>
                     <img className={styles.pictureArea1} src={rhodesIland} />
