97803f5750a0e646f0019e88ac5eac1df1b03a45
tanmutian <tanmutian@126.com>
2021-11-21 10:55:47 +0800

    2021.11.21

--- project/arknights/codeReact/src/pages/user/index.js
+++ project/arknights/codeReact/src/pages/user/index.js
@@ -18,18 +18,7 @@ const User =()=>{
         <div> 
             <div>
                 {test}
-                {/* 初始状态是数组
-                        [{
-                            name:'张三',
-                            age:16,
-                        }，
-                        {
-                            name:'李四',
-                            age:17,
-                        }]
-                点击按钮，往数组里面新增一项，值随意,在html里面是数组中name的以逗号分隔的字符串
-
-                张三，李四  */}
+                自己命题并完成，必须使用对象数组，对对象数组中的一个或者多个值在页面中进行展示。开放性
             </div> 
             
             <div className={styles.button} onClick={onClickButton}> 
--- project/arknights/codeReact/src/pages/userTest/index.js
+++ project/arknights/codeReact/src/pages/userTest/index.js
@@ -9,28 +9,55 @@ import styles from './index.less';
 // @connect(({ user }) => ({ user }))
 const User =()=>{
     const [arrayTest, setArrayTest]=useState([{
-        name : '1',
-        no : 1
+        name: 'Tony',
+        highestRank: 'Gold-4',
+        presentRank:'Gold-2',
+        job: 'leader and commander'
+    },{
+        name: 'Victor',
+        highestRank: 'Gold-3',
+        presentRank:'Gold-2',
+        job: 'break through'
+    },{
+        name: 'Steven',
+        highestRank: 'Gold-2',
+        presentRank:'Gold-1',
+        job: 'free man and scondary sniper'
+    },{
+        name: 'Henry',
+        highestRank: 'Gold-4',
+        presentRank:'Gold-1',
+        job: 'primary sniper'
+    },{
+        name: 'Eric',
+        highestRank: 'Gold-1',
+        presentRank:'Silver-Elite',
+        job: 'break through'
     }]);
-    const [theAnswer, setTheAnswer]=useState(arrayTest[0].name);
+    const [sentence, setSentence]=useState([
+        'The name of the team member is: ',
+        'The highest rank he reached was: ',
+        'The present rank of him is: ',
+        'The job he does in the team is: '
+    ])
+    const [theAnswer, setTheAnswer]=useState('');
+    const [count, setCount]=useState(0);
     const firstButton =()=>{        
-        let valueFormed = Math.random();
-        let newValue = {
-            name : valueFormed,
-            no : arrayTest.length+1
-        }
-        let newArrayTest=[...arrayTest];
-        newArrayTest.push(newValue);
-        setArrayTest(newArrayTest);
-        let answer='';
-        for(let i=0;i<newArrayTest.length;i++){
-            answer=answer+newArrayTest[i].name+',';
-        }
-        setTheAnswer(answer);
+        let firstSentence=sentence[0]+arrayTest[count].name+'. ';
+        let secondSentence=sentence[1]+arrayTest[count].highestRank+'. ';
+        let thirdSentence=sentence[2]+arrayTest[count].presentRank+'. ';
+        let forthSentence=sentence[3]+arrayTest[count].job+'. ';
+        let newAnswer= firstSentence+secondSentence+thirdSentence+forthSentence;
+        setTheAnswer(newAnswer);
+        if(count==4){
+            setCount(0);
+        }else{
+            setCount(count+1);
+        }      
     }
     return (
         <div>
-            <div>
+            <div className={styles.text}>
                 {theAnswer}
             </div>
             <div className={styles.button} onClick={firstButton}>
