2721b217d0a0407380189b62cbd49c76696e42b3
tanmutian <tanmutian@126.com>
2022-01-09 11:23:45 +0800

    2022.1.9

--- project/arknights/codeReact/src/pages/userTest/index.js
+++ project/arknights/codeReact/src/pages/userTest/index.js
@@ -12,21 +12,41 @@ const User =()=>{
 
     const [test,setTest]=useState([{
         name:'Royce',
-        age:19,
-        cource:'Psychology'
+        class:2,
+        score:88
     },{
 
         name:'Keivn',
-        age:19,
-        cource:'management'
+        class:1,
+        score:85
     },{
         name:'Steven',
-        age:19,
-        cource:'mechanical Engineering'
+        class:5,
+        score:76
     },{
         name:'Sunny',
-        age:18,
-        cource:'Electronic and electrical engineering'
+        class:3,
+        score:96
+    },{
+        name:'Linda',
+        class:1,
+        score:93
+    },{
+        name:'Tony',
+        class:1,
+        score:91
+    },{
+        name:'Wu',
+        class:8,
+        score:98
+    },{
+        name:'Nicola',
+        class:1,
+        score:97
+    },{
+        name:'Mike',
+        class:2,
+        score:92
     }]);
     const onClickButton =()=>{
         let newTest=[];
@@ -35,33 +55,44 @@ const User =()=>{
         }
         setTest(newTest);
     }
-    const horizon =()=>{
-        let functionReturnObjectHtml=[];
+    const classRanking =()=>{
+        let rankList=[]
         for(let i=0;i<test.length;i++){
-            functionReturnObjectHtml.push(
-                <div className={styles.item}>
-                    <div className={styles.name}>
-                        {test[i].name}
-                    </div>
-                    <div className={styles.large}>
-                        <div className={styles.age}>
-                            {test[i].age}
-                        </div>
-                        <div className={styles.cource}>
-                            {test[i].cource}
-                        </div>
-                    </div>
-                </div>
-            );
+            for(let j=0;j<=i;j++){
+                if(test[j].score<test[j+1].score){
+                    let temp
+                    temp=
+                }
+            }
         }
-        return functionReturnObjectHtml;
+        let classRank=[];
+        for(let i=0;i<test.length;i++){
+            if(test[i].class==1){
+                classRank.push(
+                    <div className={styles.theClass}>
+                        <div className={styles.name}>
+                            {test[i].name}
+                        </div>
+                        <div classNmae={styles.content}>
+                            <div className={styles.rank}>
+                                {classRank.length+1}
+                            </div>
+                            <div className={styles.name}>
+                                {test[i].name}
+                            </div>
+                        </div>
+                    </div>
+                );
+            }            
+        }
+        return classRank;
     }
     
     
 
     return (
         <div className={styles.global}>
-            {horizon()}
+            
         </div>
     
     )
