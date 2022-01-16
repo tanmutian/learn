1f39decd32550eab633154e98caacd62836272b3
tanmutian <tanmutian@126.com>
2022-01-16 10:55:52 +0800

    2022.1.16

--- project/arknights/codeReact/src/pages/userTest/index.js
+++ project/arknights/codeReact/src/pages/userTest/index.js
@@ -11,42 +11,43 @@ import styles from './index.less';
 const User =()=>{
 
     const [test,setTest]=useState([{
-        name:'Royce',
-        class:2,
-        score:88
-    },{
-
         name:'Keivn',
         class:1,
-        score:85
-    },{
-        name:'Steven',
-        class:5,
-        score:76
-    },{
-        name:'Sunny',
-        class:3,
-        score:96
+        score:99,
     },{
         name:'Linda',
         class:1,
-        score:93
+        score:93,
     },{
         name:'Tony',
         class:1,
-        score:91
-    },{
-        name:'Wu',
-        class:8,
-        score:98
+        score:91,
     },{
         name:'Nicola',
         class:1,
-        score:97
-    },{
+        score:88,
+    }]);
+
+    const [grade,setGrade]=useState([{
         name:'Mike',
         class:2,
-        score:92
+        score:98,
+    },{
+        name:'Wu',
+        class:8,
+        score:96,
+    },{
+        name:'Sunny',
+        class:3,
+        score:92,
+    },{
+        name:'Steven',
+        class:5,
+        score:88,
+    },{
+        name:'Royce',
+        class:2,
+        score:76,
     }]);
     const onClickButton =()=>{
         let newTest=[];
@@ -55,19 +56,22 @@ const User =()=>{
         }
         setTest(newTest);
     }
+
     const classRanking =()=>{
-        let rankList=[]
-        for(let i=0;i<test.length;i++){
-            for(let j=0;j<=i;j++){
-                if(test[j].score<test[j+1].score){
-                    let temp
-                    temp=
-                }
-            }
-        }
+        //let rankList=[]
+       // for(let i=0;i<test.length;i++){
+        //    for(let j=0;j<i;j++){
+         //       if(test[j].score<test[j+1].score){
+          //          let temp={};
+         //           temp=test[j];
+           //         test[j]=test[j+1];
+           //         test[j+1]=temp
+           //     }
+           // }
+       // }
         let classRank=[];
         for(let i=0;i<test.length;i++){
-            if(test[i].class==1){
+            //if(test[i].class==1){
                 classRank.push(
                     <div className={styles.theClass}>
                         <div className={styles.name}>
@@ -75,24 +79,75 @@ const User =()=>{
                         </div>
                         <div classNmae={styles.content}>
                             <div className={styles.rank}>
-                                {classRank.length+1}
+                                {i+1}
                             </div>
-                            <div className={styles.name}>
-                                {test[i].name}
+                            <div className={styles.score}>
+                                {test[i].score}
                             </div>
+                            <div className={styles.clear}>
+                            </div>
+                        </div>
+                        <div className={styles.clear}>
                         </div>
                     </div>
                 );
-            }            
+            //}            
         }
         return classRank;
     }
     
+    const gradeRanking =()=>{
+       // let rankList=[]
+       // for(let i=0;i<grade.length;i++){
+         //   for(let j=0;j<=i;j++){
+         //       if(test[j].score<test[j+1].score){
+         //           let temp={};
+         //           temp=test[j];
+          //          test[j]=test[j+1];
+         //           test[j+1]=temp
+          //      }
+        //    }
+       // }
+        let gradeRanking=[];
+        for(let i=0;i<grade.length;i++){
+            gradeRanking.push(
+                <div className={styles.theGrade}>
+                    <div className={styles.info}>
+                        <div className={styles.name2}>
+                            {grade[i].name}
+                        </div>
+                        <div className={styles.class2}>
+                            class:{grade[i].class}
+                        </div>
+                        <div className={styles.clear}>
+                        </div>
+                    </div>
+                    <div className={styles.rank2}>
+                        {i+1}
+                    </div>
+                    <div className={styles.clear}>
+                    </div>
+                </div>
+            )
+        }
+        return gradeRanking
+    }
     
 
     return (
         <div className={styles.global}>
-            
+            <div className={styles.totalArea}>
+                <div className={styles.classRank}>
+                    {classRanking()}
+                </div>
+
+                <div className={styles.gradeRank}>
+                    {gradeRanking()}
+                </div>
+                
+                <div className={styles.clear}>
+                </div>
+            </div>          
         </div>
     
     )
